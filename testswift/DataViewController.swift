575b8e1769f7c41dda56894722c3e3e0182db435
bobbynie <bobby@bobbyniedemacbook-pro.local>
2016-05-02 21:21:29 +0800

    Initial Commit

---
+++ testswift/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  testswift
+//
+//  Created by bobbynie on 16/5/2.
+//  Copyright © 2016年 bobbynie. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
